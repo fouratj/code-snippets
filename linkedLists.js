bb5d05fc95610261e9dd6fc7882a3089c17d8a44
Fourat Janabi <fouratj@gmail.com>
2017-03-21 12:14:54 -0300

    fixed bug

--- linkedLists.js
+++ linkedLists.js
@@ -1,3 +1,8 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+
 const addTwoNumbers = function(l1, l2) {
     let answer = [], response = [];
     
@@ -17,20 +22,21 @@ const addTwoNumbers = function(l1, l2) {
             retrieveLinks(list.next, array); //recursive algorithm to continue extracting until null is found
         }
     }
-    
+
     function createList(node) {
+        
         var x = response.splice(0, 1)[0];
         
         node.val = x;
         node.next = { val: '', next: ''};
-        
-        
+
         if (response.length === 0) {
             node.next = null;
             return node; //exits recursive sub-loop
-        }        
+        }
+
         node.next = createList({}); //creates final null
-        console.log(node)
+        
         return node;   //exits function
     }
     
@@ -42,14 +48,10 @@ const addTwoNumbers = function(l1, l2) {
                 .reverse()
                 .map(item => parseInt(item));
 
-    console.log(response)
-    //response = answer.reduce( (pre, curr) => (pre + curr), 0)
-    //response = answer//.toString().split('').reverse().map(item => parseInt(item));
-    let nodeList = createList({})
+    let nodeList = createList({});
     
     return nodeList;
 };
-
 
 const l1 = { val: 2, next: {val: 3, next: {val: 4, next: null }}};
 const l2 = { val: 5, next: {val: 6, next: {val: 2, next: null }}};
