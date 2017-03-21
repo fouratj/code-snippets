700b49b4dff39f86acaffd1cbc807307878466d8
Fourat Janabi <fouratj@gmail.com>
2017-03-21 11:44:11 -0300

    updated linkedlists.js

--- linkedLists.js
+++ linkedLists.js
@@ -1,5 +1,4 @@
-
-var addTwoNumbers = function(l1, l2) {
+const addTwoNumbers = function(l1, l2) {
     let answer = [], response = [];
     
     for (let i = 0; i < arguments.length; i++) {
@@ -11,16 +10,15 @@ var addTwoNumbers = function(l1, l2) {
         
         array.push(list.val);
         
-        if (list && list.next === null) {
-            let red = array.reverse().join('');
-            answer.push(red);
+        if (list && list.next === null) { 
+            let red = array.reverse().join(''); 
+            answer.push(red); //all links retrieved and won't recurse further
         } else {
-            retrieveLinks(list.next, array);    
+            retrieveLinks(list.next, array); //recursive algorithm to continue extracting until null is found
         }
     }
     
     function createList(node) {
-        console.log(response, response.length)
         var x = response.splice(0, 1)[0];
         
         node.val = x;
@@ -29,28 +27,30 @@ var addTwoNumbers = function(l1, l2) {
         
         if (response.length === 0) {
             node.next = null;
-            return node;
-        }
-        
-        node.next = createList({});
-        
-        return node
-    
-        
-        
-            
+            return node; //exits recursive sub-loop
+        }        
+        node.next = createList({}); //creates final null
+        console.log(node)
+        return node;   //exits function
     }
     
-    answer = answer.map(item => parseInt(item));
-    response = answer.reduce( (pre, curr) => (pre + curr), 0)
-    response = response.toString().split('').reverse().map(item => parseInt(item));
+    response = answer
+                .map(item => parseInt(item))
+                .reduce( (pre, curr) => (pre + curr), 0)
+                .toString()
+                .split('')
+                .reverse()
+                .map(item => parseInt(item));
+
+    console.log(response)
+    //response = answer.reduce( (pre, curr) => (pre + curr), 0)
+    //response = answer//.toString().split('').reverse().map(item => parseInt(item));
     let nodeList = createList({})
-    console.log(nodeList)
     
     return nodeList;
 };
 
 
-var l1 = { val: 2, next: {val: 3, next: {val: 4, next: null}}};
-var l2 = { val: 5, next: {val: 6, next: {val: 2, next: null}}};
-addTwoNumbers(l1, l2);
+const l1 = { val: 2, next: {val: 3, next: {val: 4, next: null }}};
+const l2 = { val: 5, next: {val: 6, next: {val: 2, next: null }}};
+console.log(addTwoNumbers(l1, l2));
