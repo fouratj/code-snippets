16db43e72a69acfe4575c301a777a8f61735df7a
Fourat Janabi <fouratj@gmail.com>
2017-05-05 15:05:51 -0300

    spacing

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,8 @@
 const fizzBuzz = function (n) {
     let data = [];
-    
+
     for (let i = 1; i <= n; i++ ) {
+
         if ( i % 3 === 0 && i % 5 !== 0) {
             data.push('Fizz');
         } else if ( i % 5 === 0 && i % 3 !== 0) {
@@ -11,9 +12,10 @@ const fizzBuzz = function (n) {
         } else {
             data.push(i.toString());
         }
-    }  
+        
+    }
+
     return data;
-     
 };
 
 console.log(fizzBuzz(1));
