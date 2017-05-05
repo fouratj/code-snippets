ec69e602f9a72658b73919cde0f21808c4f1b34b
Fourat Janabi <fouratj@gmail.com>
2017-05-05 15:05:15 -0300

    removed unnecessary var

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,7 @@
-const fizzBuzz = function (a) {
-    let n = parseInt(a);
-
+const fizzBuzz = function (n) {
     let data = [];
+    
     for (let i = 1; i <= n; i++ ) {
-        
         if ( i % 3 === 0 && i % 5 !== 0) {
             data.push('Fizz');
         } else if ( i % 5 === 0 && i % 3 !== 0) {
