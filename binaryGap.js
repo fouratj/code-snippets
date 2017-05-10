0da6d21b1d62bb3b3f81212339c4fecafe221d88
Fourat Janabi <fouratj@gmail.com>
2017-05-10 20:41:11 -0300

    binaryGap finished

--- binaryGap.js
+++ binaryGap.js
@@ -1,9 +1,21 @@
+//Write a function that, given a positive integer N, returns the length of its longest binary gap. 
+//The function should return 0 if N doesn't contain a binary gap.
 
 function binaryGap (n) {
-    var num = (n >>> 0).toString(2);
-    
+    let num = (n >>> 0).toString(2).split('');
+    console.log(num)
+    let count = [0];
+
+    num.forEach(item => {
+
+        if (item === "0") {
+            count[count.length - 1]++;
+        } else if (item === "1" && count[count.length - 1] !== 0) {
+            count.push(0);
+        }
+    });
+    return Math.max(...count);
 }
 
 console.log(binaryGap(5));
-
-console.log(binaryGap(15));
+console.log(binaryGap(56502));
