5886e0273f84eff1d9b4980268a50ac55e9f74f2
Fourat Janabi <fouratj@gmail.com>
2017-05-15 11:41:13 -0300

    Added explainer

--- minJumps.js
+++ minJumps.js
@@ -1,4 +1,8 @@
-
+// A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+// Count the minimal number of jumps that the small frog must perform to reach its target.
+// Write a function:
+// function solution(X, Y, D);
+// that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
 
 function solution(X, Y, D) {
     // write your code in JavaScript (Node.js 6.4.0)
