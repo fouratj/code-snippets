efa5b800a43f2fa2b0dbb59f5f2fbf65e347ea89
Fourat Janabi <fouratj@gmail.com>
2017-06-28 22:05:20 -0300

    arrayPartition from codility

---
+++ arrayPartition.js
@@ -0,0 +1,19 @@
+// Array Partition I
+// Given an array of 2n integers, your task is to group these integers into n pairs of integer, 
+// say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as 
+// large as possible.
+
+var arrayPairSum = function(nums) {
+    var arr = nums.sort((a,b) => ( a - b)), l = nums.length;
+    var myset = [];
+    
+    for (let i = 0; i < l; i+= 2) {
+        myset.push([arr[i], arr[i+1]]);
+    }
+    
+    return myset
+                .map(item => ( Math.min(item[0], item[1])))
+                .reduce( (pre, curr) => ( pre + curr), 0);
+};
+
+console.log(arrayPairSum([1,4,3,2,6,8]));
