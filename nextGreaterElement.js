e690af81188fd3ea41460b216d3c1e2a6c10921d
Fourat Janabi <fouratj@gmail.com>
2017-07-01 17:04:56 -0300

    nextGreaterElement added

--- nextGreaterElement.js
+++ nextGreaterElement.js
@@ -1,27 +1,28 @@
+// You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
+// Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+// The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. 
+// If it does not exist, output -1 for this number.
+
 var nextGreaterElement = function(findNums, nums) {
+   var answers = [], l = nums.length; 
     
-    let answer = [];
-    
-    while (findNums > 0) {
-        let curr = findNums.shift();
-        console.log(curr);
-        let trigger = false;
-        
-        for (let i = 0; i < nums.length; i++) {
-            console.log(nums.length)
-            if (curr > nums[i]) {
-                answer.push(nums[i]);
-                trigger = true;
-                break;
-            }
+    findNums.forEach( num => {
+        let index = nums.findIndex( (item, index) => ( num == item ));
+
+        let newArr = [];
+        let elem;
+
+        if (index !== -1) {
+	        newArr = nums.slice(index + 1);
+			elem = newArr.find( item => ( num < item )) || -1;
+        } else {
+            elem = index;
         }
-        
-        if (trigger) {
-            answer.push(-1);
-        }
-    }
+
+        answers.push(elem);        
+    });
     
-    return answer;
+    return answers;
 };
 
-nextGreaterElement([4,1,2],[1,3,4,2]);
+console.log(nextGreaterElement([4,1,2], [1,3,4,2]))
