510fb47d6f94d388b3fb8149c2b866b234a38d33
Fourat Janabi <fouratj@gmail.com>
2017-07-31 11:59:24 -0300

    Added the original problem description

--- reshapeTheMatrix.js
+++ reshapeTheMatrix.js
@@ -4,6 +4,15 @@
 // Status: Accepted
 // Runtime: 225 ms
 
+// You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing 
+// the row number and column number of the wanted reshaped matrix, respectively.
+
+// The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing
+//  order as they were.
+
+// If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; 
+// Otherwise, output the original matrix.
+
 var matrixReshape = function(nums, r, c) {
     if ( nums.length === 0 || (r * c) !== (nums.length * nums[0].length) ) {
         return nums;
