e98d2b0fc781b5bbc45325b6ffe4f9dc26a07b60
Fourat Janabi <fouratj@gmail.com>
2018-04-28 21:13:40 -0300

    initial commit

---
+++ quickSort.js
@@ -0,0 +1,13 @@
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  const pivot = arr.pop()
+  const left = quickSort(arr.filter(e => e < pivot))
+  const right = quickSort(arr.filter(e => e >= pivot))
+
+  return [...left, pivot, ...right]
+}
+
+console.log(quickSort([1, 5, 2, 7, 4, 9, 3, 6]))
