d6e2f91894755901864b54c14957bfef236f3c53
Fourat Janabi <fouratj@gmail.com>
2018-05-01 10:59:26 -0300

    initial commit

---
+++ morseCodeWords.js
@@ -0,0 +1,51 @@
+// 83 / 83 test cases passed.
+// Status: Accepted
+// Runtime: 56 ms
+
+var uniqueMorseRepresentations = function(words) {
+  const wordsInMorse = {
+      a: ".-",
+      b: "-...",
+      c: "-.-.",
+      d: "-..",
+      e: ".",
+      f: "..-.",
+      g: "--.",
+      h: "....",
+      i: "..",
+      j: ".---",
+      k: "-.-",
+      l: ".-..",
+      m: "--",
+      n: "-.",
+      o: "---",
+      p: ".--.",
+      q: "--.-",
+      r: ".-.",
+      s: "...",
+      t: "-",
+      u: "..-",
+      v: "...-",
+      w: ".--",
+      x: "-..-",
+      y: "-.--",
+      z: "--..",
+  };
+  const transformations = [];
+  
+  words.forEach(w => {
+      const word = w.split('');
+      let wordInMorse = '';
+      word.forEach(wo => {
+          wordInMorse += wordsInMorse[wo];
+      });
+      transformations.push(wordInMorse);
+  });
+  
+  return transformations.filter((t, i, arr) => {
+      return i === arr.indexOf(t);
+  }).length
+  
+};
+
+console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]));
