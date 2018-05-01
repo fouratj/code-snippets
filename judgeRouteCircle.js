56041e069904cc25937148eacfec285d790b6368
Fourat Janabi <fouratj@gmail.com>
2018-05-01 11:08:29 -0300

    add problem

--- judgeRouteCircle.js
+++ judgeRouteCircle.js
@@ -1,3 +1,12 @@
+// Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+
+// The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down).
+// The output should be true or false representing whether the robot makes a circle.
+
+// 62 / 62 test cases passed.
+// Status: Accepted
+// Runtime: 64 ms
+
 /**
  * @param {string} moves
  * @return {boolean}
